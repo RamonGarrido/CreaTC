839931e866efaa93a886df74c3791f6847d3ae04
Ramon Garrido Maldonado <e.videoqa030.tid@telefonica.com>
2025-01-07 09:52:18 +0100

    07-01-2025 #3

--- ConfluenceCreaTC.py
+++ ConfluenceCreaTC.py
@@ -1007,12 +1007,10 @@ def modificarTesCaseId(key, monitorizacion, modificar):
                                 # Solo modificamos la columna 11 (Test Case ID)
                                 if contador < len(key):
                                     # Crear un enlace HTML con el valor de la clave
-                                    enlace = f'<a href="https://jira.tid.es/browse/{
-                                        key[contador]}">{key[contador]}</a>'
+                                    enlace = f'<a href="https://jira.tid.es/browse/{key[contador]}">{key[contador]}</a>'
                                     celda.string = ''  # Limpiar el contenido de la celda
                                     # Insertar el enlace
-                                    celda.append(BeautifulSoup(
-                                        enlace, 'html.parser'))
+                                    celda.append(BeautifulSoup(enlace, 'html.parser'))
                                     contador += 1
 
                     tabla_html_actualizada = str(tabla_original)
